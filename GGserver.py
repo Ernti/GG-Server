662f07f7c20c2e646e69cbbbdc38b8a3ed11a7b5
udakak <udakak@gmail.com>
2013-12-11 20:26:22 +0100

    clients can disconnect without the server going crazy

--- GGserver.py
+++ GGserver.py
@@ -17,20 +17,26 @@ class AcceptConnectionThread(Thread):
         sock.listen(5)
         while not stop_requested:
             conn, addr = sock.accept()
-            clients.append({'conn': conn, 'addr': addr})
-            receive_thread = ReceiveTread(conn, addr)
+            client = {'conn': conn, 'addr': addr}
+            clients.append(client)
+            client_id = clients.index(client)
+            receive_thread = ReceiveTread(conn, addr, client_id)
             receive_thread.start()
             
 class ReceiveTread(Thread):
     
-    def __init__(self, conn, addr):
+    def __init__(self, conn, addr, client_id):
         Thread.__init__(self)
         self.conn = conn
         self.addr = addr
+        self.client_id = client_id
     
     def run(self):
         while not stop_requested:
             data = self.conn.recv(1024)
+            if not data:
+                clients.pop(self.client_id)
+                break
             print(data)
             
 
