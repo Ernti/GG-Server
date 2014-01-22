e655c8bef8a4d8d627842eb40345bc920650e6c4
udakak <udakak@gmail.com>
2014-01-22 19:04:40 +0100

    removed SendThread because it didn't really make sense

--- ggs/client.py
+++ ggs/client.py
@@ -22,21 +22,6 @@ class ReceiveThread(Thread):
                 self.client.alive = False
 
 
-class SendThread(Thread):
-
-    def __init__(self, client):
-        Thread.__init__(self)
-        self.client = client
-
-    def run(self):
-        while self.client.alive:
-            for message in self.client.message_queue:
-                try:
-                    self.client.conn.send(message.encode())
-                except socket.error:
-                    self.client.alive = False
-
-
 class Client(object):
 
     def __init__(self, server, conn, addr):
@@ -44,14 +29,14 @@ class Client(object):
         self.conn = conn
         self.addr = addr
         self.alive = True
-        self.message_queue = []
         self.receive_thread = ReceiveThread(self)
         self.receive_thread.start()
-        self.send_thread = SendThread(self)
-        self.send_thread.start()
 
     def send(self, message):
-        self.message_queue.append(message)
+        try:
+            self.conn.send(message.encode())
+        except socket.error:
+            self.alive = False
 
     def handle(self, message):
         if message['type'] == 'playermoved':
