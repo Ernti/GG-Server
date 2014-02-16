8c373868ed659a66736abd73e013503c52912f08
tore <tore.mueller@googlemail.com>
2014-02-16 17:30:49 +0100

    fixed JSON Extra Data error with regular Expressions

--- ggs/client.py
+++ ggs/client.py
@@ -1,5 +1,6 @@
 import json
 import socket
+import re
 from threading import Thread
 
 
@@ -13,11 +14,14 @@ class ReceiveThread(Thread):
         while self.client.alive:
             try:
                 data = self.client.conn.recv(1024)
-                if data:
-                    self.client.handle(json.loads(data.decode()))
-                else:
-                    self.client.handle({'type': 'removespaceobject'})
-                    self.client.alive = False
+                for match_group in re.finditer("\{([^{}]+)\}", data.decode()):
+
+                    data_json = json.loads('{' + match_group.group(1) + '}')
+                    if data:
+                        self.client.handle(data_json)
+                    else:
+                        self.client.handle({'type': 'removespaceobject'})
+                        self.client.alive = False
 
             except socket.error:
                 self.client.handle({'type': 'removespaceobject'})
@@ -49,4 +53,4 @@ class Client(object):
             self.server.player_action(message, self)
         if message['type'] == 'sendchatmessage':
             self.server.player_action(message, self)
-        print(message)
+        #print(message)
